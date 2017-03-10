300edd93953ac9e5d6029f4c363399088a5c4cfd
Stivali Serna <stivaliserna08@gmail.com>
2017-03-10 13:58:10 +0100

    Make assets URLs' relative

--- app/app.config.js
+++ app/app.config.js
@@ -8,27 +8,27 @@ angular.module('hubGourmetApp').config([
 function routerConfig ($routeProvider) {
   $routeProvider
    .when('/products', {
-     templateUrl: 'products/products.tpl.html',
+     templateUrl: 'app/products/products.tpl.html',
      controller: 'productsController',
      controllerAs: 'products'
    })
    .when('/checkout', {
-     templateUrl: 'checkout/checkout.tpl.html',
+     templateUrl: 'app/checkout/checkout.tpl.html',
      controller: 'checkoutController',
      controllerAs: 'checkout'
    })
    .when('/admin/products', {
-     templateUrl: 'admin-products/admin-products.tpl.html',
+     templateUrl: 'app/admin-products/admin-products.tpl.html',
      controller: 'adminProductsController',
      controllerAs: 'admin'
    })
    .when('/admin/product/add', {
-     templateUrl: 'admin-add/admin-add.tpl.html',
+     templateUrl: 'app/admin-add/admin-add.tpl.html',
      controller: 'adminAddController',
      controllerAs: 'add'
    })
    .when('/admin/product/edit/:id', {
-     templateUrl: 'admin-products/admin-products.tpl.html',
+     templateUrl: 'app/admin-products/admin-products.tpl.html',
      controller: 'adminProductsController',
      controllerAs: 'admin'
    })
