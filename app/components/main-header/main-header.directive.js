c389c60afe59299567df50d31665aae859c15f2b
Stivali Serna <stivaliserna08@gmail.com>
2017-03-21 09:26:32 +0100

    Make header interactive to use it in different views

--- app/components/main-header/main-header.directive.js
+++ app/components/main-header/main-header.directive.js
@@ -5,10 +5,13 @@ angular
   .directive('mainHeader', [
     'ShoppingCartService',
     'authService',
+    'localStorageService',
+    '$location',
+    '$log',
     mainHeader
   ])
 
-function mainHeader (ShoppingCartService, authService) {
+function mainHeader (ShoppingCartService, authService, localStorageService, $location, $log) {
   return {
     templateUrl: 'app/components/main-header/main-header.directive.tpl.html',
     controller: mainHeaderController,
@@ -19,5 +22,20 @@ function mainHeader (ShoppingCartService, authService) {
     let vm = this
 
     vm.authService = authService
+    vm.showAdminComponents = showAdminComponents
+    vm.hideClientComponents = hideClientComponents
+    vm.showCart = showCart
+
+    function showAdminComponents () {
+      return !!localStorageService.get('id_token')
+    }
+
+    function hideClientComponents () {
+      return !localStorageService.get('id_token')
+    }
+
+    function showCart () {
+      return !!localStorageService.get('id_token') && ($location.path() === '/products')
+    }
   }
 }
